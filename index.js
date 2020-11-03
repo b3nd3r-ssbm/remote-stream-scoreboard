39b06d7a84b5740618a3d408ef8ad2351753245f
b3nd3r-ssbm <67122986+b3nd3r-ssbm@users.noreply.github.com>
2020-11-03 12:46:36 -0500

    Revert "Revert "Add update notifications""
    This reverts commit 8d2986d04bdc49576f7e43a057beefea300e6b71.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const {app, BrowserWindow, remote} = require('electron');
+const {shell, app, BrowserWindow, remote, Notification} = require('electron');
 const {newSession,update,closeSession} = require('./server.js');
+const {check, getLatest} = require('./check-for-updates.js');
 
 let code;
 let time = 0;
@@ -21,6 +22,16 @@ function init() {
   newSession().then((data) => {
     setCode(JSON.parse(data).code);
 	setStart();
+  });
+  
+  getLatest().then((data) => {
+    console.log(data);
+    if(isTagGreater(data, "v"+remote.app.getVersion())) {
+      check().then((data) => {
+         document.getElementById("updater").hidden = false;
+	     document.getElementById("updater").setAttribute("onclick","shell.openExternal('"+data.url+"')");
+      });
+    }
   });
 }
 
@@ -44,4 +55,35 @@ function setStart() {
 function setCode(data) {
   code = data;
   document.getElementById("code").innerHTML = data;
+}
+
+function tagInt(tagName) {
+  let majorStr = tagName.substring(tagName.indexOf("v")+1,tagName.indexOf("."));
+  major = parseInt(majorStr);
+  tagName = tagName.substring(tagName.indexOf("v"+majorStr)+majorStr.length+2);
+  let minorStr=tagName.substring(0,tagName.indexOf("."));
+  let minor = parseInt(minorStr);
+  tagName = tagName.substring(tagName.indexOf("."));
+  let patchStr = tagName.substring(1);
+  let patch = parseInt(patchStr);
+  return [major,minor,patch];
+}
+
+function isTagGreater(tag1, tag2){
+  tag1 = tagInt(tag1);
+  tag2 = tagInt(tag2);
+  if(tag1[0] > tag2[0]) {
+    return true;
+  }
+  if(tag1[0] == tag2[0]) {
+    if(tag1[1] > tag2[1]){
+      return true;
+    }
+    if(tag1[1] == tag2[1]) {
+      if(tag1[2] > tag2[2]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
